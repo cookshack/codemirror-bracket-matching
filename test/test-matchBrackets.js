bad2f369a56cc13cfcd301c55ede627c14d1281c
Matt Mundell <matt@mundell.me>
2025-03-29 20:54:49 +0200

    Test empty brackets

--- test/test-matchBrackets.js
+++ test/test-matchBrackets.js
@@ -5,6 +5,21 @@ function state(text) {
     return EditorState.create({ doc: text });
 }
 describe("matchBrackets", () => {
+    it("(·)", () => {
+        ist(matchBrackets(state("()"), 1, 1), null);
+    });
+    it("(·) ←", () => {
+        ist(matchBrackets(state("()"), 1, -1), null);
+    });
+    it("·()", () => {
+        let match = matchBrackets(state("()"), 0, 1);
+        ist(match.matched, true);
+        ist(match.end.from, 1);
+        ist(match.end.to, 2);
+    });
+    it("·() ←", () => {
+        ist(matchBrackets(state("()"), 0, -1), null);
+    });
     it("(a·b)", () => {
         ist(matchBrackets(state("(ab)"), 2, 1), null);
     });
@@ -32,6 +47,18 @@ describe("matchBrackets", () => {
     });
 });
 describe("matchEnclosingBrackets", () => {
+    it("(·)", () => {
+        let match = matchEnclosingBrackets(state("()"), 1);
+        ist(match.matched, true);
+        ist(match.end.from, 1);
+        ist(match.end.to, 2);
+    });
+    it("·()", () => {
+        ist(matchEnclosingBrackets(state("()"), 0), null);
+    });
+    it("()·", () => {
+        ist(matchEnclosingBrackets(state("()"), 2), null);
+    });
     it("(a·b)", () => {
         let match = matchEnclosingBrackets(state("(ab)"), 2);
         ist(match.matched, true);
--- test/test-matchBrackets.ts
+++ test/test-matchBrackets.ts
@@ -8,6 +8,28 @@ function state(text) {
 }
 
 describe("matchBrackets", () => {
+  it("(·)", () => {
+    ist(matchBrackets(state("()"), 1, 1),
+        null)
+  })
+
+  it("(·) ←", () => {
+    ist(matchBrackets(state("()"), 1, -1),
+        null)
+  })
+
+  it("·()", () => {
+    let match = matchBrackets(state("()"), 0, 1)
+    ist(match.matched, true)
+    ist(match.end.from, 1)
+    ist(match.end.to, 2)
+  })
+
+  it("·() ←", () => {
+    ist(matchBrackets(state("()"), 0, -1),
+        null)
+  })
+
   it("(a·b)", () => {
     ist(matchBrackets(state("(ab)"), 2, 1),
         null)
@@ -44,6 +66,23 @@ describe("matchBrackets", () => {
 })
 
 describe("matchEnclosingBrackets", () => {
+  it("(·)", () => {
+    let match = matchEnclosingBrackets(state("()"), 1)
+    ist(match.matched, true)
+    ist(match.end.from, 1)
+    ist(match.end.to, 2)
+  })
+
+  it("·()", () => {
+    ist(matchEnclosingBrackets(state("()"), 0),
+        null)
+  })
+
+  it("()·", () => {
+    ist(matchEnclosingBrackets(state("()"), 2),
+        null)
+  })
+
   it("(a·b)", () => {
     let match = matchEnclosingBrackets(state("(ab)"), 2)
     ist(match.matched, true)
